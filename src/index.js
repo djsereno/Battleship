f42d5090c2c951773595a43244f11352ef30a805
djsereno <djsereno91@gmail.com>
2023-07-18 22:54:10 -0700

    Add Ship factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+const Ship = (length) => {
+  const size = length;
+  let damage = 0;
+  let alive = true;
+
+  const hit = () => {
+    // Returns true if the hit sinks the ship
+    if (!alive) return undefined;
+    damage += 1;
+    if (damage === size) alive = false;
+    return !alive;
+  };
+
+  const isSunk = () => alive;
+
+  return {
+    size,
+    damage,
+    hit,
+    isSunk,
+  };
+};
+
+const carrier = Ship(5);
+carrier.hit();
