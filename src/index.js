bfb05fca0e79a7ba9ecaa096046ad3d0d33e5d64
djsereno <djsereno91@gmail.com>
2023-07-24 00:49:17 -0700

    Implement basic turn switching

--- src/index.js
+++ src/index.js
@@ -6,11 +6,40 @@ import Player from './player';
   const BOARD_SIZE = 10;
   const player1 = Player('Player 1', true, BOARD_SIZE);
   const player2 = Player('Player 2', true, BOARD_SIZE);
+  const player1Heading = document.querySelector('#player-1>h2');
+  const player2Heading = document.querySelector('#player-2>h2');
+  let attacker = player1;
+  let defender = player2;
+
   player1.initBoard();
   player2.initBoard();
 
-  const initGridDOM = (playerDivId) => {
-    const playerDiv = document.querySelector(playerDivId);
+  const getCellCoord = (target) => {
+    if (!target.classList.contains('grid-cell')) return undefined;
+    const row = target.getAttribute('data-row');
+    const col = target.getAttribute('data-col');
+    return [row, col];
+  };
+
+  const changeTurn = () => {
+    [attacker, defender] = [defender, attacker];
+    player1Heading.classList.toggle('current');
+    player2Heading.classList.toggle('current');
+  };
+
+  const handleAttack = (event, player) => {
+    if (player === attacker) return;
+    const cell = event.currentTarget;
+    const target = getCellCoord(cell);
+    const hitShip = player.board.receiveAttack(target);
+    if (hitShip === undefined) return;
+    if (!hitShip) cell.classList.add('miss');
+    if (hitShip) cell.classList.add('hit');
+    changeTurn();
+  };
+
+  const initGridDOM = (divId, player) => {
+    const playerDiv = document.querySelector(divId);
     const grid = playerDiv.querySelector('.grid-container');
     for (let i = 0; i < BOARD_SIZE; i += 1) {
       for (let j = 0; j < BOARD_SIZE; j += 1) {
@@ -18,11 +47,17 @@ import Player from './player';
         cell.classList.add('grid-cell');
         cell.setAttribute('data-row', i);
         cell.setAttribute('data-col', j);
+
+        if (player.board.getCell([i, j]).ship)
+          cell.innerText = player.board.getCell([i, j]).ship.key;
+
+        cell.addEventListener('click', (e) => handleAttack(e, player));
+
         grid.appendChild(cell);
       }
     }
   };
 
-  initGridDOM('#player-1');
-  initGridDOM('#player-2');
+  initGridDOM('#player-1', player1);
+  initGridDOM('#player-2', player2);
 })();
