7ccd012fddbef859da94e9cde88fb79a0a54c0b1
djsereno <djsereno91@gmail.com>
2023-07-23 20:21:59 -0700

    Begin setting up UI, add grids to DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const Ship = require('./ship');
+import './normalize.css';
+import './styles.css';
+import Player from './player';
 
-const ship = Ship('BAT');
-ship.hit();
-ship.hit();
-ship.hit();
+(() => {
+  const BOARD_SIZE = 10;
+  const player1 = Player('Player 1', true, BOARD_SIZE);
+  const player2 = Player('Player 2', true, BOARD_SIZE);
+  player1.initBoard();
+  player2.initBoard();
+
+  const initGridDOM = (playerDivId) => {
+    const playerDiv = document.querySelector(playerDivId);
+    const grid = playerDiv.querySelector('.grid-container');
+    for (let i = 0; i < BOARD_SIZE; i += 1) {
+      for (let j = 0; j < BOARD_SIZE; j += 1) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        cell.setAttribute('data-row', i);
+        cell.setAttribute('data-col', j);
+        grid.appendChild(cell);
+      }
+    }
+  };
+
+  initGridDOM('#player-1');
+  initGridDOM('#player-2');
+})();
