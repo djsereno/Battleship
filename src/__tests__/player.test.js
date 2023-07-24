309abecdd5a5922f0546428cfc6c8947dd3aca98
djsereno <djsereno91@gmail.com>
2023-07-23 19:20:20 -0700

    Add player getTarget, name, and tests

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -1,22 +1,57 @@
 import Player from '../player';
-import { SHIP_KEYS } from '../globals';
+import { SHIP_KEYS, SHIP_SIZES } from '../globals';
 
-for (let i = 0; i < 10; i += 1) {
-  describe(`Test randopm player ${i}:`, () => {
-    const player = Player();
-    test('Player isAI to be true', () => {
-      expect(player.isAI).toBeTruthy();
+for (let i = 0; i < 1; i += 1) {
+  const player1 = Player('Player 1');
+  const player2 = Player('Player 2', false);
+  const { size } = player1.board;
+
+  describe('Initialize players:', () => {
+    test(`${i}: name/isAI/board/isAlive: Player 1 is AI and has gameboard`, () => {
+      expect(player1.name).toBe('Player 1');
+      expect(player1.isAI).toBeTruthy();
+      expect(player1.board).toBeDefined();
+      expect(player1.isAlive()).toBeTruthy();
     });
-    test('Player has a defined gameboard', () => {
-      expect(player.board).toBeDefined();
+    test(`${i}: name/isAI/board/isAlive: Player 2 is human and has gameboard`, () => {
+      expect(player2.name).toBe('Player 2');
+      expect(player2.isAI).toBeFalsy();
+      expect(player2.board).toBeDefined();
+      expect(player2.isAlive()).toBeTruthy();
+    });
+  });
+
+  describe('Setup boards and check target acquisition:', () => {
+    beforeAll(() => {
+      player1.initBoard();
+      player2.initBoard();
     });
 
-    player.initBoard();
-    describe.each(SHIP_KEYS)('All ships are placed:', (ship) => {
-      test(`${ship} is defined`, () => {
-        expect(player.board.ships[ship]).toBeDefined();
-      });
+    test(`${i}: initBoard: All ships are properly placed`, () => {
+      const shipSizeTotal = Object.values(SHIP_SIZES).reduce((a, b) => a + b, 0);
+      const ships = Object.fromEntries(SHIP_KEYS.map((ship) => [ship, 0]));
+      let empties = 0;
+
+      player1.board.array.forEach((row) =>
+        row.forEach((cell) => {
+          if (cell.ship) ships[cell.ship.key] += 1;
+          if (!cell.ship) empties += 1;
+        }),
+      );
+      expect(ships).toEqual(SHIP_SIZES);
+      expect(empties).toBe(size * size - shipSizeTotal);
     });
-    player.board.prettyPrint(`Player 1-${i}:`);
+
+    test(`${i}: getTarget: Target is defined and attack was received`, () => {
+      for (let j = 0; j < size * size; j += 1) {
+        const target = player1.getTarget();
+        const attackResult = player2.board.receiveAttack(target);
+        expect(target).toBeDefined();
+        expect(attackResult).toBeDefined();
+      }
+    });
+    test(`${i}: isAlive: Player is not alive after firing all possible shots`, () => {
+      expect(player2.isAlive()).toBe(false);
+    });
   });
 }
--- src/player.js
+++ src/player.js
@@ -5,26 +5,45 @@ function randomInt(int) {
   return Math.floor(Math.random() * int);
 }
 
-const Player = (ai = true) => {
+const Player = (playerName, ai = true) => {
+  const name = playerName;
   const isAI = ai;
   const gameboard = Gameboard();
 
-  const initBoard = () => {
-    if (isAI) {
-      SHIP_KEYS.forEach((ship) => {
-        let valid = false;
-        do {
-          const row = randomInt(10);
-          const col = randomInt(10);
-          const dir = ['H', 'V'][randomInt(2)];
-          valid = gameboard.placeShip(ship, [row, col], dir);
-        } while (!valid);
-      });
+  const availableMoves = [];
+  for (let i = 0; i < gameboard.size; i += 1) {
+    for (let j = 0; j < gameboard.size; j += 1) {
+      availableMoves.push([i, j]);
     }
+  }
+
+  const getTarget = () => {
+    const rand = randomInt(availableMoves.length);
+    const [target] = availableMoves.splice(rand, 1);
+    return target;
+  };
+
+  const isAlive = () => !gameboard.checkGameOver();
+
+  const initBoard = () => {
+    SHIP_KEYS.forEach((ship) => {
+      let valid = false;
+      do {
+        const row = randomInt(10);
+        const col = randomInt(10);
+        const dir = ['H', 'V'][randomInt(2)];
+        valid = gameboard.placeShip(ship, [row, col], dir);
+      } while (!valid);
+    });
   };
 
   return {
+    getTarget,
     initBoard,
+    isAlive,
+    get name() {
+      return name;
+    },
     get isAI() {
       return isAI;
     },
